b156626331bb431523eb4b14abc220b1339e617e
JoãoSouz4 <joaosouz8@gmail.com>
2026-07-20 23:15:21 -0300

    .

--- src/view/components/header.jsx
+++ src/view/components/header.jsx
@@ -3,20 +3,27 @@ import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 import { FaWhatsapp } from "react-icons/fa";
 export function Header() {
-  return (
-    <Link
-      className="bg-blue-700 p-4 sticky top-0 flex justify-between md:justify-center z-100 "
-      to={"/"}
-    >
-      <div className="md:w-250 md:flex md:justify-between md:px-6 flex justify-between w-full">
-        <div className="flex items-center gap-2 text-white text-2xl">
-          <img className="w-10 " src={logo} alt="logo" />
-        </div>
+  const message =
+    "Olá. Vim pelo site da garantia. Gostaria de tirar uma dúvida...";
 
-        <div className="border w-fit border-white rounded-xl p-2 px-4- flex items-center justify-center text-xs text-white gap-2">
+  return (
+    <>
+      <div className="bg-blue-700 p-4 sticky top-0 flex justify-between items-center md:justify-center gap-12 z-100">
+        <Link className=" " to={"/"}>
+          <div className="flex items-center gap-2 text-white text-2xl">
+            <img className="w-10 " src={logo} alt="logo" />
+          </div>
+        </Link>
+
+        <a
+          href={`https://wa.me/69993116619?text=${encodeURIComponent(message)}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="border w-fit border-white rounded-xl p-2 px-4- flex items-center justify-center text-xs text-white gap-2"
+        >
           <FaWhatsapp className="text-sm" /> Entre em contato
-        </div>
+        </a>
       </div>
-    </Link>
+    </>
   );
 }
