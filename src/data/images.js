ad710aca2296fae1ffdf42dba4a2e711aa010b9b
JoãoSouz4 <joaosouz8@gmail.com>
2026-08-10 22:32:29 -0300

    .

--- src/data/images.js
+++ src/data/images.js
@@ -2,7 +2,7 @@ import brokenScreen from "../assets/brokenScreen.png";
 import scratchedScreen from "../assets/scratchedScreen.png";
 import stripingScreen1 from "../assets/stripingScreen1.png";
 import spottedScreen1 from "../assets/spottedScreen1.png";
-import toqueFantasma from "../assets/toqueFantasma.png";
+import toqueFantasma from "../assets/toqueFantasma.webp";
 import semTouch from "../assets/semTouch.png";
 import telapiscando from "../assets/telapiscando.png";
 import telaapagada from "../assets/telaapagada.png";
